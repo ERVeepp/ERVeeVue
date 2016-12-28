92e708a61809d87495b9d0245c40a3d194312aa5
王伟 <736354882@qq.com>
2016-12-28 18:37:20 +0800

    123

--- src/components/my/components/b.vue
+++ src/components/my/components/b.vue
@@ -5,7 +5,22 @@
 </template>
 
 <script>
-
+    export default{
+        mounted:function(){
+            this.$http.post('http://localhost:3000/my', {}, {
+				headers: {},
+				emulateJSON: true
+			}).then(function(response) {
+                console.log(response)
+				// 这里是处理正确的回调
+				//this.articles = response.data.subjects
+				// this.articles = response.data["subjects"] 也可以
+			}, function(response) {
+				// 这里是处理错误的回调
+				console.log(response)
+			});
+        }
+    }
 </script>
 
 <style lang="css">
